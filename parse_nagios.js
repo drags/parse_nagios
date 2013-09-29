58c7a187501c78b8c8f2fa02837b2a2123ef9f59
Tim Sogard <src@timsogard.com>
2013-09-29 13:07:28 -0700

    package.json, argparse, toggle JSON output

--- parse_nagios.js
+++ parse_nagios.js
@@ -1,12 +1,23 @@
 var fs = require('fs');
+var argparse = require('argparse').ArgumentParser;
 var parse = require('./parser/services.js');
 
-console.log(process.argv);
+var argp = new argparse({
+	version: '0.0.1',
+	addHelp: true,
+	description: 'Parse Nagios configuration'
+});
+argp.addArgument(['-j', '--json'], { nargs: 0, help: 'Output as JSON' });
+argp.addArgument(['files'], { action: 'append', help: 'Files to process' });
+var args = argp.parseArgs();
+console.dir(args)
 
-var files = process.argv.slice(2);
-
+var files = args.files
 files.forEach(function(file){
 	var dat = fs.readFileSync(file, 'utf8');
 	var out = parse.parse(dat);
+	if (args.json) {
+		var out = JSON.stringify(out);
+	}
 	console.log(out);
 });
